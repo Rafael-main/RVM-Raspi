b8d1b1b40647bf33ca366e45ceb23873a82cca05
Rafael-main <rafaeladrian.tibudan1201@g.msuiit.edu.ph>
2023-06-29 09:11:01 +0800

    firebase for raspi

--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 import time
 import threading
+import uuid
+import random
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import RPi.GPIO as GPIO
-import MFRC522
+from simplemfrc522 import SimpleMFRC522
 
-# Initialize RFID reader
-MIFAREReader = MFRC522.MFRC522()
+# # Initialize RFID reader
+reader = SimpleMFRC522()
 
 # Initialize Firebase credentials and Firestore client
-cred = credentials.Certificate("path/to/serviceAccountKey.json")  # Replace with your service account key path
+cred = credentials.Certificate("./rvm-fire-firebase-adminsdk-tmfzs-dece4493b4.json")  # Replace with your service account key path
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Main loop
-try:
-    while True:
-        # Scan for cards
-        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-        # If a card is detected
-        if status == MIFAREReader.MI_OK:
-            # Get the UID of the card
-            (status, uid) = MIFAREReader.MFRC522_Anticoll()
+def read_rfid():
+    # Main loop
+    try:
+        while True:
+            # Scan for cards
+            id, text = reader.read()
 
             # Convert UID to string
-            uid_str = ''.join(str(i) for i in uid)
+            if id:
+                uid_str = ''.join(f'user-{str(uuid.uuid4())[:5]}')
 
-            # Send UID to Firestore
-            doc_ref = db.collection(u'rfid_scans').document()
-            doc_ref.set({u'uid': uid_str})
+                # Send UID to Firestore
+                doc_ref = db.collection(u'rfid_scans').document()
+                doc_ref.set({u'uid': uid_str})
 
-            print("UID scanned and sent to Firestore: {}".format(uid_str))
+                print("UID scanned and sent to Firestore: {}".format(uid_str))
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+
+
+def add():
+    # Convert UID to string
+    doc_num = random.randrange(1000000,9999999)
+    uid_str = ''.join(f'user-{str(uuid.uuid4())[:5]}')
+    data = {
+        'id': uid_str,
+        'imageUrl': 'm',
+        'points': 100,
+        'public': True,
+        'rfidtag': '789c',
+        'username': 'ricci'
+    }
+    # Send UID to Firestore
+    doc_ref = db.collection('Users').document(f'{doc_num}')
+    doc_ref.set(data)
+
+def update(doc_num):
+    user_doc =  db.collection("Users").document(f'{doc_num}')
+    update_data = {'rfidtag': 'mnl9132'}
+    user_doc.update(update_data)
+
+update(1833901)
+
+
+
+# add()
